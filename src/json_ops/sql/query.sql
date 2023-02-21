c2844e33adf1660007bc3720cdc96b2a6c286481
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2023-02-20 21:03:48 -0400

    add abstract to the output

--- src/json_ops/sql/query.sql
+++ src/json_ops/sql/query.sql
@@ -2,11 +2,14 @@ select json_build_object(
   'id', id, 
   'name', name, 
   'date', date, 
+  'abstract', abstract,
   'cast', movie_casts.cast,
   'movies_in_series', other_movies.movies_in_series
 ) data from
-(select id, name, date, parent_id 
-from movies where id=$1) movie left join lateral (
+(select movies.id, name, date, parent_id, abstract
+from movies 
+left join movie_abstracts_en on movies.id=movie_abstracts_en.movie_id
+where id=$1) movie left join lateral (
   select json_agg(
       json_build_object('role', role, 
                         'person_id', person_id, 
@@ -18,9 +21,10 @@ from movies where id=$1) movie left join lateral (
   select json_agg(
     json_build_object('id', m.id,
                       'name', name,
-                      'date', date
+                      'date', date,
+                      'abstract', abstract
                       ) 
   ) movies_in_series
-  from movies m
+  from movies m left join movie_abstracts_en ma on ma.movie_id = m.id
   where (m.id=movie.parent_id or m.parent_id=movie.parent_id) and m.id != movie.id
 ) other_movies on true;
